5c2f354d5505387d0e3ecf6b674434946da78b8b
chongniannian <915325011@qq.com>
2022-02-09 15:29:31 +0800

    one

---
+++ say/say/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  say
+//
+//  Created by chongniannian on 2022/2/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
